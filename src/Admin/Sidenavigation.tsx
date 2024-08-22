1c9c25c5dfc768e1e6cce017e61c3af897b3b77b
udeshi <udeshigamage92@gmail.com>
2024-08-22 12:34:56 +0530

    Modified ui of side nav

--- src/Admin/Sidenavigation.tsx
+++ src/Admin/Sidenavigation.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 import { MdPeopleOutline } from "react-icons/md";
 import pico3 from "../assets/Screenshot 2025-02-09 220935.png";
-import { IoCallOutline, IoPersonOutline } from "react-icons/io5";
+import {
+  IoCallOutline,
+  IoLogOut,
+  IoPersonOutline,
+  IoSettings,
+} from "react-icons/io5";
 import { AiOutlineFundProjectionScreen } from "react-icons/ai";
 import { FaRegUserCircle } from "react-icons/fa";
 
@@ -9,28 +14,36 @@ const Sidenavigation = () => {
   return (
     <div className="w-1/5 bg-white ">
       <div className="flex flex-col">
-        <div className="flex flex-row position-relative font-serif font-semibold gap-2 m-5 text-blue-950 text-lg">
+        <div className="flex flex-row position-relative font-serif font-semibold gap-2 m-4 text-blue-950 text-lg">
           <img src={pico3} alt="logo" />
         </div>
-        <div className="flex flex-row position-relative font-serif font-semibold gap-2 m-5 text-blue-950 text-lg">
-          <MdPeopleOutline className=" " size={45} />
+        <div className="flex flex-row position-relative font-serif font-semibold gap-2 m-4 text-blue-950 text-lg">
+          <MdPeopleOutline className=" " size={40} />
           <h1 className="pt-2 ">Customers</h1>
         </div>
-        <div className="flex flex-row position-relative font-serif font-semibold gap-2 m-5 text-blue-950 text-lg">
-          <IoPersonOutline className=" " size={45} />
+        <div className="flex flex-row position-relative font-serif font-semibold gap-2 m-4 text-blue-950 text-lg">
+          <IoPersonOutline className=" " size={40} />
           <h1 className="pt-2 ">Vendors</h1>
         </div>
-        <div className="flex flex-row position-relative font-serif font-semibold gap-2 m-5 text-blue-950 text-lg">
-          <AiOutlineFundProjectionScreen className=" " size={45} />
+        <div className="flex flex-row position-relative font-serif font-semibold gap-2 m-4 text-blue-950 text-lg">
+          <AiOutlineFundProjectionScreen className=" " size={40} />
           <h1 className="pt-2 ">Projects</h1>
         </div>
-        <div className="flex flex-row position-relative font-serif font-semibold gap-2 m-5 text-blue-950 text-lg">
-          <IoCallOutline className=" " size={45} />
+        <div className="flex flex-row position-relative font-serif font-semibold gap-2 m-4 text-blue-950 text-lg">
+          <IoCallOutline className=" " size={40} />
           <h1 className="pt-2 ">CIA</h1>
         </div>
-        <div className="flex flex-row position-relative font-serif font-semibold gap-2 m-5 text-blue-950 text-lg">
-          <FaRegUserCircle className=" " size={45} />
+        <div className="flex flex-row position-relative font-serif font-semibold gap-2 m-4 text-blue-950 text-lg">
+          <FaRegUserCircle className=" " size={35} />
           <h1 className="pt-2 ">Systemuser</h1>
+        </div>
+        <div className="flex flex-row position-relative font-serif font-semibold gap-2 mt-14 ml-5 text-blue-950 text-lg">
+          <IoSettings className=" " size={35} />
+          <h1 className="pt-2 ">Settings</h1>
+        </div>
+        <div className="flex flex-row position-relative font-serif font-semibold gap-2  m-5 text-blue-950 text-lg">
+          <IoLogOut className=" " size={35} />
+          <h1 className="pt-2 ">Logout</h1>
         </div>
       </div>
     </div>
