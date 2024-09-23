1848031c9aef7c00010f2b60ddcc9407e392269d
udeshi <udeshigamage92@gmail.com>
2024-09-23 10:34:56 +0530

    removed projcet services and add props

--- src/Admin/Project/Projectmajor.tsx
+++ src/Admin/Project/Projectmajor.tsx
@@ -3,20 +3,22 @@ import Projectitemmain from "../Projectitem/Projectitemmain";
 import Projecttestmain from "../Projecttest/Projecttestmain";
 import ProjectServicemain from "../Projectservice/ProjectServicemain";
 import Projectciamain from "../ProjectCIA/Projectciamain";
+import { useLocation } from "react-router-dom";
 
 const Projectmajor = () => {
   const [selectedTable, setSelectedTable] = useState("Project Item");
+  const location = useLocation();
+  const { item } = location.state || {};
 
   const renderTable = () => {
     switch (selectedTable) {
       case "Project Item":
-        return <Projectitemmain />;
-      case "Services":
-        return <ProjectServicemain />;
+        return <Projectitemmain project={item} />;
+
       case "Tests":
-        return <Projecttestmain />;
+        return <Projecttestmain project={item} />;
       case "CIA":
-        return <Projectciamain />;
+        return <Projectciamain project={item} />;
       default:
         return null;
     }
@@ -25,11 +27,11 @@ const Projectmajor = () => {
   return (
     <div className="bg-[#B4D6E4] h-screen">
       <div className="flex flex-row justify-evenly gap-2 ">
-        {["Project Item", "Services", "Tests", "CIA"].map((item) => (
+        {["Project Item", "Tests", "CIA"].map((item) => (
           <button
             key={item}
             className={`bg-white text-[#183642] p-5 rounded-lg w-1/3 m-2 ${
-              selectedTable === item ? "bg-[#8C41DC] text-white" : ""
+              selectedTable === item ? "bg-[#8C41DC] text-black" : ""
             }`}
             onClick={() => setSelectedTable(item)}
           >
